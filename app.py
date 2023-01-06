a785a6d8f9bafbc53e32b891a1c8fda6f353bc31
Seshu Sai <105602908+iseshu@users.noreply.github.com>
2023-01-06 18:44:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,18 @@
 from flask import Flask,request,jsonify
 from flask_cors import CORS
 from seleniumwire import webdriver
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from webdriver_manager.chrome import ChromeDriverManager
 from bs4 import BeautifulSoup
 import os
 
-options = Options()
-options.add_argument('--disable-dev-shm-usage')
 
 app = Flask(__name__)
+cors = CORS(app, resources={r"/*": {'Access-Control-Allow-Origin': '*'}})
+op = webdriver.ChromeOptions()
+op.add_argument('--headless')
+op.add_argument('--no-sandbox')
+op.add_argument('--disable-dev-sh-usage')
+
+
 
 def get(pageSource,mobile):
     try:
@@ -28,7 +28,6 @@ def get(pageSource,mobile):
     except:
         return {"status":False}
 
-    
 @app.route("/")
 def home():
     return {"status":True}
@@ -36,25 +35,26 @@ def home():
 
 @app.route("/get")
 def do():
-        a= request.args.get("id")
-        driver  = driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
-        driver.get("https://payonline.narayanagroup.com/")
-        id_num = driver.find_element_by_id("txtUid")
-        id_num.send_keys(int(a))
-        btn = driver.find_element_by_id("BtnStuGetData")
-        btn.click()
-        pageSource = driver.page_source
-        amount = driver.find_element_by_id("txtCourseFee")
-        amount.send_keys(1280)
-        btn1 = driver.find_element_by_id("btnsubmit")
-        btn1.click()
-        for reqest in driver.requests:
-            if reqest.url == "https://secure.payu.in/_payment":
-                phone = reqest.params['phone']
-        data = get(pageSource,phone)
-        return jsonify(data)
+    a= request.args.get("id")
+    driver = webdriver.Chrome(chrome_options=op)
+    driver.maximize_window()
+    driver.get("https://payonline.narayanagroup.com/")
+    id_num = driver.find_element_by_id("txtUid")
+    id_num.send_keys(int(a))
+    btn = driver.find_element_by_id("BtnStuGetData")
+    btn.click()
+    pageSource = driver.page_source
+    amount = driver.find_element_by_id("txtCourseFee")
+    amount.send_keys(1280)
+    btn1 = driver.find_element_by_id("btnsubmit")
+    btn1.click()
+    for reqest in driver.requests:
+        if reqest.url == "https://secure.payu.in/_payment":
+            phone = reqest.params['phone']
+    data = get(pageSource,phone)
+    return jsonify(data)
+
 
 
 if __name__ == "__main__":
-    app.run()
-
+    app.run(host="0.0.0.0")
